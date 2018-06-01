aed7e0790296394ba2bf7f35a62fe60eefe880a6
Patrick Wigger <pwigger@gmail.com>
2018-06-01 15:44:06 +0200

    different color for runners

--- TrackMyTime/src/pages/home/home.ts
+++ TrackMyTime/src/pages/home/home.ts
@@ -25,11 +25,22 @@ export class HomePage {
 
   addPlayers() {
     for (let i = 1; i < 5; i++) {
-      this.firebaseService.addPlayer(this.players.value["player" + i], "#00d181").then(() => {
+      this.firebaseService.addPlayer(this.players.value["player" + i], this.getColor()).then(() => {
       });
     }
 
     this.navCtrl.push(GoViewPage, {
     });
   }
+  getColor(){
+   
+    console.log("royal")
+    var random=Math.floor((Math.random() * 5) + 1);
+    if(random==1){return "c1 button button-md button-default button-default-md button-block button-block-md button-large button-large-md"}
+    if(random==2){return "c2 button button-md button-default button-default-md button-block button-block-md button-large button-large-md";}
+    if(random==3){return "c3 button button-md button-default button-default-md button-block button-block-md button-large button-large-md";}
+    if(random==4){return "c4 button button-md button-default button-default-md button-block button-block-md button-large button-large-md";}
+    else {return "c5 button button-md button-default button-default-md button-block button-block-md button-large button-large-md";}
+ 
+}
 }
